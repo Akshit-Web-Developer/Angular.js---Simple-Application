d0d9b85fb8d9fa39b56b1ff66f1a8d15315d5ba1
Aks <akshitpeddaboodi@gmail.com>
2017-11-29 11:51:40 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,45 @@
+(function() {
+    var app = angular.module("githubViewer", []);
+
+    var MainController = function($scope, $http, $interval){
+
+        var onUserComplete = function(response){
+            $scope.user = response.data;
+            $http.get($scope.user.repos_url)
+              .then(onRepos, onError);
+        };
+        
+         var onRepos = function(response){
+            $scope.repos = response.data;
+        };
+
+        var onError = function(reason){
+            $scope.error = "Couldn't fetch the data !!";
+        };
+
+        var decrementCountdown = function(){
+            $scope.countdown -= 1;
+            if($scope.countdown < 1){
+                $scope.search($scope.username);
+            }
+        };
+
+        var startCountdown = function(){
+            $interval(decrementCountdown, 1000, $scope.countdown);
+        }
+        
+        $scope.search = function(username){
+        
+            $http.get("https://api.github.com/users/" + username)
+            .then(onUserComplete, onError);
+        };
+
+        $scope.message = "Github Viewer!";
+        $scope.username = "Angular";
+        $scope.repoSortOrder = "-stargazers_count";
+        $scope.countdown = 5;
+        startCountdown();
+    }
+    app.controller("MainController", MainController);
+
+})();
